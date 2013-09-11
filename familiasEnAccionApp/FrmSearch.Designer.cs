130f078e765595bc0f2f04881769a366ea7a2abb
Edwin Piragauta <edwin.piragauta@skaphe.com>
2013-09-11 14:15:53 -0500

    Comit inicial

---
+++ familiasEnAccionApp/FrmSearch.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace familiasEnAccionApp
+{
+  partial class FrmSearch
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+    private System.Windows.Forms.MainMenu mainMenu1;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmSearch));
+      this.mainMenu1 = new System.Windows.Forms.MainMenu();
+      this.mnuItemSearch = new System.Windows.Forms.MenuItem();
+      this.mnuItemClear = new System.Windows.Forms.MenuItem();
+      this.mnuItemOptions = new System.Windows.Forms.MenuItem();
+      this.mnuItemOpenDB = new System.Windows.Forms.MenuItem();
+      this.mnuItemClose = new System.Windows.Forms.MenuItem();
+      this.lblDoc = new System.Windows.Forms.Label();
+      this.txtSearch = new System.Windows.Forms.TextBox();
+      this.dataGridResult = new System.Windows.Forms.DataGrid();
+      this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      this.pictureBox2 = new System.Windows.Forms.PictureBox();
+      this.SuspendLayout();
+      // 
+      // mainMenu1
+      // 
+      this.mainMenu1.MenuItems.Add(this.mnuItemSearch);
+      this.mainMenu1.MenuItems.Add(this.mnuItemClear);
+      this.mainMenu1.MenuItems.Add(this.mnuItemOptions);
+      this.mainMenu1.MenuItems.Add(this.mnuItemClose);
+      // 
+      // mnuItemSearch
+      // 
+      this.mnuItemSearch.Text = "Buscar";
+      this.mnuItemSearch.Click += new System.EventHandler(this.mnuItemSearch_Click);
+      // 
+      // mnuItemClear
+      // 
+      this.mnuItemClear.Text = "Borrar";
+      this.mnuItemClear.Click += new System.EventHandler(this.mnuItemClear_Click);
+      // 
+      // mnuItemOptions
+      // 
+      this.mnuItemOptions.MenuItems.Add(this.mnuItemOpenDB);
+      this.mnuItemOptions.Text = "Opciones";
+      // 
+      // mnuItemOpenDB
+      // 
+      this.mnuItemOpenDB.Text = "Seleccionar BD";
+      this.mnuItemOpenDB.Click += new System.EventHandler(this.menuItem1_Click);
+      // 
+      // mnuItemClose
+      // 
+      this.mnuItemClose.Text = "Cerrar";
+      this.mnuItemClose.Click += new System.EventHandler(this.mnuItemClose_Click);
+      // 
+      // lblDoc
+      // 
+      this.lblDoc.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
+      this.lblDoc.Location = new System.Drawing.Point(11, 4);
+      this.lblDoc.Name = "lblDoc";
+      this.lblDoc.Size = new System.Drawing.Size(205, 19);
+      this.lblDoc.Text = "Placa:";
+      // 
+      // txtSearch
+      // 
+      this.txtSearch.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Regular);
+      this.txtSearch.Location = new System.Drawing.Point(6, 24);
+      this.txtSearch.Name = "txtSearch";
+      this.txtSearch.Size = new System.Drawing.Size(229, 23);
+      this.txtSearch.TabIndex = 1;
+      // 
+      // dataGridResult
+      // 
+      this.dataGridResult.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+      this.dataGridResult.Location = new System.Drawing.Point(9, 52);
+      this.dataGridResult.Name = "dataGridResult";
+      this.dataGridResult.Size = new System.Drawing.Size(224, 156);
+      this.dataGridResult.TabIndex = 2;
+      // 
+      // openFileDialog1
+      // 
+      this.openFileDialog1.FileName = "openFileDialog1";
+      // 
+      // pictureBox1
+      // 
+      this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+      this.pictureBox1.Location = new System.Drawing.Point(3, 214);
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size(50, 51);
+      // 
+      // pictureBox2
+      // 
+      this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+      this.pictureBox2.Location = new System.Drawing.Point(185, 223);
+      this.pictureBox2.Name = "pictureBox2";
+      this.pictureBox2.Size = new System.Drawing.Size(52, 42);
+      // 
+      // FrmSearch
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+      this.AutoScroll = true;
+      this.ClientSize = new System.Drawing.Size(240, 268);
+      this.Controls.Add(this.pictureBox2);
+      this.Controls.Add(this.pictureBox1);
+      this.Controls.Add(this.dataGridResult);
+      this.Controls.Add(this.txtSearch);
+      this.Controls.Add(this.lblDoc);
+      this.Menu = this.mainMenu1;
+      this.Name = "FrmSearch";
+      this.Text = "Busquedas";
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label lblDoc;
+    private System.Windows.Forms.MenuItem mnuItemSearch;
+    private System.Windows.Forms.MenuItem mnuItemClear;
+    private System.Windows.Forms.MenuItem mnuItemOptions;
+    private System.Windows.Forms.MenuItem mnuItemClose;
+    private System.Windows.Forms.TextBox txtSearch;
+    private System.Windows.Forms.DataGrid dataGridResult;
+    private System.Windows.Forms.MenuItem mnuItemOpenDB;
+    private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    private System.Windows.Forms.PictureBox pictureBox1;
+    private System.Windows.Forms.PictureBox pictureBox2;
+  }
+}
+
---
+++ familiasEnAccionApp/FrmSearch.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+
+namespace familiasEnAccionApp
+{
+  public partial class FrmSearch : Form
+  {
+    private SQLiteConnection _connection;
+    private string _sDataSource;
+    private SQLiteDataReader sqlReader;
+
+    public FrmSearch()
+    {
+      InitializeComponent();
+    }
+
+    private bool connectDB()
+    { 
+      _connection = new SQLiteConnection();
+      _connection.ConnectionString = "Data Source=" + _sDataSource + ";New=false;Compress=True;Synchronous=Off";
+      try
+      {
+        _connection.Open();
+        return true;
+      }
+      catch (Exception ex)
+      {
+        System.Console.WriteLine(ex.Message);
+        return false;
+      }
+    }
+
+    private void menuItem1_Click(object sender, EventArgs e)
+    {
+      openFileDialog1.Filter = "*.sqlite|*.sqlite";
+      if (openFileDialog1.ShowDialog() == DialogResult.OK)
+      {
+        _sDataSource = openFileDialog1.FileName;
+      }
+
+      bool b = connectDB();
+      if (!b)
+      {
+        MessageBox.Show("Problemas conectando a la BD");
+      }
+      return;
+
+    }
+
+    private void mnuItemSearch_Click(object sender, EventArgs e)
+    {
+      if (_connection == null)
+      {
+        MessageBox.Show("Conectese a una BD");
+        return;
+      }
+
+      
+      try
+      {
+        SQLiteCommand command;
+        command = _connection.CreateCommand();
+        String sTabla = "apoyo_logistico";
+        String sCampo = "placa";
+        ////command.CommandText = "SELECT id, no_localid, barrio_cam, direccion, nombre_com FROM establecimientos_prost";
+        string sSQL = "SELECT * FROM " + sTabla + " WHERE " + sCampo + " = ? ";
+        //string sSQL2 = "SELECT * FROM FAMILIAS_EN_ACCION WHERE documento like ?  limit 10";
+        command.CommandText = sSQL;
+        SQLiteParameter param = command.CreateParameter();
+        param.Value = txtSearch.Text;
+        command.Parameters.Add(param);
+
+        dataGridResult.DataSource = null;
+        SQLiteDataReader reader = command.ExecuteReader();
+        if (!reader.HasRows)
+        {
+          //command.Dispose();
+          //command = _connection.CreateCommand();
+          //command.CommandText = sSQL2;
+          //param = command.CreateParameter();
+          //param.Value = txtSearch.Text;
+          //command.Parameters.Add(param);
+          //reader.Close();
+          //reader = command.ExecuteReader();
+          if (!reader.HasRows)
+          {
+            MessageBox.Show("no existe informacion para el documento ingresado" );
+            return;
+          }
+        }
+        sqlReader = reader;
+        
+        FillDataset();
+      }
+      catch (MissingMethodException ex)
+      {
+        MessageBox.Show("Error creando la conexion. verifique con el administrador.\n" + ex.Message);
+        return;
+      }                      
+
+    }
+
+    /// <summary>
+    /// Llena el objeto Dataset
+    /// </summary>
+    public void FillDataset()
+    {
+
+      int iNumeroColumnas;
+      object[] clObjetos;
+      DataTable dataTabla = new DataTable();
+      DataColumn dataColumn;
+      DataRow dataRow = dataTabla.NewRow();
+
+      // Variables para cambiar la apariencia del DataGrid.
+      DataGridTableStyle dgTableStyle = new DataGridTableStyle();
+      dgTableStyle.MappingName = dataTabla.TableName;
+      DataGridColumnStyle colStayle = new DataGridTextBoxColumn();
+
+      while (sqlReader.Read())
+      {
+        // Se crean las columnas.
+
+        iNumeroColumnas = sqlReader.FieldCount;
+        clObjetos = new Object[iNumeroColumnas];
+
+        if (dataTabla.Columns.Count <= 0)
+        {
+          for (int i = 0; i < iNumeroColumnas; i++)
+          {
+            dataColumn = new DataColumn();
+            dataColumn.ColumnName = sqlReader.GetName(i);
+            dataColumn.DataType = Type.GetType("System.String");
+            dataColumn.ReadOnly = true;
+            dataColumn.AllowDBNull = true;
+            dataTabla.Columns.Add(dataColumn);
+
+            colStayle = new DataGridTextBoxColumn();
+            colStayle.HeaderText = dataColumn.ColumnName;
+            colStayle.MappingName = dataColumn.ColumnName;
+
+            /* Modificamos el ancho de las columnas
+             * cuando el Nº de columnas es menor que 3.*/
+            if (iNumeroColumnas == 1 && i == 0)
+            {
+              colStayle.Width = 210;
+            }
+            else //(iNumeroColumnas==2)
+            {
+              if (i == 0) colStayle.Width = 50;
+              else if (i == 1) colStayle.Width = 150;
+              else colStayle.Width = 70;
+            }
+
+            dgTableStyle.GridColumnStyles.Add(colStayle);
+          }
+        }
+        sqlReader.GetValues(clObjetos);
+        dataTabla.Rows.Add(clObjetos);
+      }
+      dataGridResult.DataSource = dataTabla;
+      Cursor.Current = Cursors.Default;
+    }
+
+    private void mnuItemClear_Click(object sender, EventArgs e)
+    {
+      txtSearch.Text = "";
+    }
+
+    private void mnuItemClose_Click(object sender, EventArgs e)
+    {
+      this.Close();
+    }
+
+
+  }
+}
